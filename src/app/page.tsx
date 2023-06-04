a76a42fa30998c3eb737501bd18e081d0ced1ff9
trustmitt <mainmirror14@gmail.com>
2023-06-04 23:21:41 +0900

    feat : 랜딩 페이지 구조 대충 잡음 - tailwind css 변수 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import FillButton from "@/components/@common/FillButton";
+import Image from "next/image";
 
 export default function Home() {
-    return <div>test</div>;
+    return (
+        <div className="w-full h-screen  flex justify-center items-center">
+            <div className="h-auto">
+                <div className="w-[200px] h-[200px] relative">
+                    <Image src="/logo.png" alt="Seat Hunters" fill />
+                </div>
+                <div>지하철 자리 쟁탈전 필승 전략!</div>
+                <div className="flex">
+                    <div className="text-primary">SEAT</div>
+                    <div>HUNTERS</div>
+                </div>
+                <button>Connect wallet</button>
+            </div>
+        </div>
+    );
 }
--- src/components/@common/FillButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface str {
-    string: string;
-}
-
-export default function FillButton({ string }: str) {
-    return <div>FillButton</div>;
-}
